74e6069f8b471dd6238f5d74a3517d30d363c07a
Makako08 <marcoalonso24@hotmail.com>
2023-08-07 11:17:22 -0600

    update

--- Progra1.m
+++ Progra1.m
@@ -1,9 +1,13 @@
-%Forma de meter matrices: mat1 = [1.2,23,3.4,45,56,67,7.8,89,90] o mat1 = [12 23 34 45 56 67 78 89 90]
+% Forma de meter matrices: mat1 = [1.2,23,3.4,45,56,67,7.8,89,90] o mat1 = [12 23 34 45 56 67 78 89 90]
 %
-%Matrices traspuestas: mat1 = [12 23 34 45 56 67 78 89 90; 
-%                              54 8 2 8 54 5.2 65 6.4 84.5; 
-%                              4 84 6 32 65.4 7 20 14 5.5]
-%Concatenando matrices transpuestas:  A = [[1 4 7]' [2 5 8]' [3 6 9]']
-%who para saber las variables
-%Para ver los componentes de la matriz uso mat1(5) o mat1(3,4) para
-%matrices de varias dimensiones
+% Matrices traspuestas: mat1 = [12 23 34 45 56 67 78 89 90; 
+%                               54 8 2 8 54 5.2 65 6.4 84.5; 
+%                               4 84 6 32 65.4 7 20 14 5.5]
+% Concatenando matrices transpuestas:  A = [[1 4 7]' [2 5 8]' [3 6 9]']
+% who: para saber las variables
+% Para ver los componentes de la matriz uso mat1(5) o mat1(3,4) para
+% matrices de varias dimensiones
+% Matrices de ceros: zero(2,3)
+% Matrices de unos: ones(2,3)
+% dos puntos (:): for escodido, correr índices del primero al último, se
+% puede cambiar el paso
