138f34642abb4f9856231c317cf5811ce03b7395
Makako08 <52816894+Makako08@users.noreply.github.com>
2023-08-28 11:17:40 -0600

    Update factLU.m

--- factLU.m
+++ factLU.m
@@ -1,1 +1,18 @@
 function [L, U] = factLU(A)
+    [n, m] = size(A);
+    U = A;
+    L = eye(n);
+
+    %Ciclo para iterar por toda la matriz
+    for k =1:n-1
+        %Recorro filas
+        for i=k+1:n
+            %Recorro columnas de derecha a izquierda para evitar problemas
+            %de sustiitución de valores
+            for j=n:-1:k
+               U(i,j) = U(i,j)-(U(i,k)/U(k,k))*U(k,j);
+               L(i,j)= (U(i,k)/U(k,k));
+            end
+        end
+    end
+end
